d828286d1a06af7452a006eeb7699638e972d96d
Silmi Khashaish Fillah <silmi.k.fillah@gmail.com>
2023-06-26 14:11:51 +0700

    Add Feature Login Non-Gherkin Test

---
+++ Object Repository/Login/emailpassinvalid.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>emailpassinvalid</name>
+   <tag></tag>
+   <elementGuidId>ca4f8f4e-62b3-47a2-9a4b-cc66ae11d52c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@text=&quot;Email atau kata sandi salah&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
